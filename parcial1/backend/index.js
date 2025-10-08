5542941e7aac4ec220a229e8f0d36274f7287eda
Hernan <hernanocaranza@hotmail.com.ar>
2025-10-07 23:24:46 -0300

    se agregan dos endpoints para la gestion de usuarios

--- parcial1/backend/index.js
+++ parcial1/backend/index.js
@@ -1,16 +1,34 @@
 const express = require('express')
 const cors = require('cors')
-const db = require("./db");
+const db = require("./db")
 
 const PORT = 3000
 
 const app = express()
 
+app.use(express.json())
 app.use(cors())
 
 //Endpoints
 app.get('/', (req, res)=>{
-    res.send('Parcial')
+    res.send("Parcial")
+})
+
+// endpoint para listar los usuarios
+app.get('/usuarios', async (req, res)=>{
+    let [filas] = await db.query('select * from usuarios;')
+    res.send(filas)
+})
+
+// endpoint para registro de usuarios
+app.post('/usuarios', async (req, res)=>{
+    const { nombre, usuario , password, rol } = req.body
+
+    let [filas] = await db.query(
+        "insert into usuarios (nombre, usuario, password, rol) values (?, ?, ?, ?)", 
+        [nombre, usuario, password, rol]
+    )
+    res.send(filas)
 })
 
 app.listen(PORT, ()=>{
