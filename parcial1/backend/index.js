49619932c48f7eed9c0c5fce8d172856c8418fe0
Maria Virginia Villarreal <mariavirg2010@hotmail.com>
2025-10-08 00:28:33 -0300

    realice endpoint de productos y ventas

--- parcial1/backend/index.js
+++ parcial1/backend/index.js
@@ -31,6 +31,42 @@ app.post('/usuarios', async (req, res)=>{
     res.send(filas)
 })
 
+//endpoint para listar productos
+app.get('/productos', async (req, res) =>{
+    let [filas] = await db.query('select * from productos')
+    res.send(filas)
+
+})
+
+//endpoint para agregar productos
+app.post('/productos', async (req, res) => {
+    const { nombre, descripcion, precio, stock } =req.body
+
+    let [filas] = await db.query(
+        "insert into productos (nombre, descripcion, precio, stock) values (?, ?, ?, ?)",
+        [nombre, descripcion, precio, stock]
+    )
+    res.send(filas)
+
+})
+
+//endpoint para listar ventas
+app.get('/ventas', async (req, res) => {
+    let [filas]=await db.query ('select * from ventas') 
+    res.send(filas)
+})
+
+//endpoint para agregar ventas
+app.post('/ventas', async (req, res) => {
+    const { total, usuario_id}=req.body
+
+    let [result] =await db.query(
+        "insert into ventas (total, usuario_id) values (?,?)",
+        [total, usuario_id]
+    )
+    res.send(result)
+})
+
 app.listen(PORT, ()=>{
     console.log('escuchando en http://localhost:'+ PORT)
 })
